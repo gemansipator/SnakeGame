ad0295dcbecf8a41c57fe15006f1894b36a3a918
gemansipator <gemansipator@gmail.com>
2022-02-13 02:43:21 +0300

    Initial commit

--- src/GameFild.java
+++ src/GameFild.java
@@ -2,6 +2,16 @@ import javax.swing.*;
 
 public class GameFild extends JPanel {
     private final int SIZE = 320;
+<<<<<<< HEAD
+=======
+    private final int DOT_SIZE = 16;
+    private final int ALL_DOTS = 400;
+
+
+    public GameFild(){
+
+    }
+>>>>>>> 3ff8d5c (Initial commit)
      
 
 }
