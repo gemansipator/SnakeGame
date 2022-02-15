287228a6e997b60a0b2e8c676c1d03eb648f8534
gemansipator <gemansipator@gmail.com>
2022-02-15 20:50:09 +0300

    2. 11:52

--- src/GameFild.java
+++ src/GameFild.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class GameFild extends JPanel {
+public class GameFild extends JPanel implements ActionListener {
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
@@ -25,6 +28,24 @@ public class GameFild extends JPanel {
         setBackground(Color.black);
         loadImages();
     }
+
+    public void initGame(){
+        dots = 3;
+        for (int i = 0; i < dots; i++) {
+            x[i] = 48 - i * DOT_SIZE;
+            y[i] = 48;
+        }
+        timer = new Timer(250,this);
+        timer.start();
+        createApple();
+    }
+
+    public void createApple(){
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
+
+    }
+
     public void loadImages(){
         ImageIcon iia = new ImageIcon("apple.png");
         apple = iia.getImage();
@@ -32,6 +53,37 @@ public class GameFild extends JPanel {
         dot = iia.getImage();
     }
 
-     
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(inGame){
+            g.drawImage(apple, appleX,appleY,this);
+            for (int i = 0; i < dots; i++) {
+                g.drawImage(dot,x[i],y[i],this);
+            }
+        }
+    }
 
+    public void move(){
+        for (int i = dots; i > 0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if (left){
+            x[0] += DOT_SIZE;
+        } if(up){
+            y[0] += DOT_SIZE;
+        } if (down){
+            y[0] += DOT_SIZE;
+        }
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (inGame){
+            move();
+        }
+        repaint();
+    }
 }
